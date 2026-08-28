9199c3f13fdbe8db496d2d35577af210999cdda7
martiparti1 <mart2004xd@gmail.com>
2026-08-28 12:35:08 +0300

    Attack registry added

--- src/combat/attacks.ts
+++ src/combat/attacks.ts
@@ -1,4 +1,20 @@
 import type { MoveFunction, MoveResult } from "@/types/combat.types";
 import type { GenericPokemon } from "@/types/pokemon.types";
 
-// export const MOVE_REGISTRY
+const STANDARD_CRIT_MULTIPLIER = 0.5;
+
+export const MOVE_REGISTRY:
+    Record<string, MoveFunction> = {
+        tackle: (attacker, target): MoveResult => {
+            const base_damage: number = 40;
+            const is_crit = Math.random() >= 0.417;
+            const message = `${attacker.name} user tackle!`;
+
+            return {
+                damage: is_crit ? base_damage * STANDARD_CRIT_MULTIPLIER : base_damage,
+                is_crit,
+                message
+            }
+        }
+
+    } satisfies Record<string, MoveFunction>;
